9dba371207949432bbbf6ca0cd3f0e5717d32d56
Patricia <82049947+Patrusky@users.noreply.github.com>
2022-08-02 21:32:25 +0200

    Añadir rutas

--- Proyecto-Marvel/src/app/app-routing.module.ts
+++ Proyecto-Marvel/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { ComicsComponent } from './componentes/comics/comics.component';
+import { FormularioComponent } from './componentes/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'comics', component:ComicsComponent},
+  {path:'contactanos', component:FormularioComponent},
+  {path:'**', redirectTo: 'home'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
