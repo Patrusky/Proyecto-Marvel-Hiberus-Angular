e753d1e40f8bd262d2586563c7e588f3976fe102
Daniel Serrano Serrano <dannyserranoserrano@gmail.com>
2022-08-02 19:35:39 +0200

    ComponentesCreados
    Home, navbar, footer, comics

--- Proyecto-Marvel/src/app/app.module.ts
+++ Proyecto-Marvel/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { ComicsComponent } from './componentes/comics/comics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ComicsComponent
   ],
   imports: [
     BrowserModule,
