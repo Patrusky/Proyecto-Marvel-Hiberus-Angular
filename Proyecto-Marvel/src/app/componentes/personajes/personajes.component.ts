0911bc2e77d83be2aeea71470f68b0944f178a3b
Patricia <82049947+Patrusky@users.noreply.github.com>
2022-08-04 20:10:01 +0200

    Llamada a personajes

--- Proyecto-Marvel/src/app/componentes/personajes/personajes.component.ts
+++ Proyecto-Marvel/src/app/componentes/personajes/personajes.component.ts
@@ -11,12 +11,27 @@ export class PersonajesComponent {
   personajes: any = [];
   nombre: string = '';
   personaje: any;
+  heroes: any[] = [];
 
   constructor(private personajeService: PersonajesService) {
     this.personajeService.getAll().subscribe((datos) => {
       this.personajes = datos.data.results;
-      console.log(datos.data.results)
+      console.log("Personajes", this.personajes);
+      //escoger a 4
+      // Filtramos los id de los personajes = [{id: 1},{id: 2}, {id: 3}] y con el includes incluimos solos los 4 que indicamos
+      let arraydeHeroes = [1011334,1017100,1010699,1011266];
+      this.heroes = this.personajes.filter( (personaje:any) => {
+        return arraydeHeroes.includes(personaje.id);
+      });
+
+      this.heroes.map( personaje => {
+        if (personaje.thumbnail.path.search('image_not_available') != -1){
+          personaje.thumbnail.path = '/assets/personajes/' + personaje.id;
+        }
+      });
+      console.log(this.heroes);
     });
+
   }
 
   buscarPersonaje() {
