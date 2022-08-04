0740cef4a1652fe04f5909b9e919e830599b6c2e
Patricia <82049947+Patrusky@users.noreply.github.com>
2022-08-04 13:43:00 +0200

    Ruta añadida y css comic adaptado

--- Proyecto-Marvel/src/app/app-routing.module.ts
+++ Proyecto-Marvel/src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
 import { ComicsComponent } from './componentes/comics/comics.component';
 import { FormularioComponent } from './componentes/formulario/formulario.component';
+import { PersonajesComponent } from './componentes/personajes/personajes.component';
+
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
   {path:'comics', component:ComicsComponent},
+  {path:'heroe', component:PersonajesComponent},
   {path:'contactanos', component:FormularioComponent},
   {path:'**', redirectTo: 'home'},
 
