47d90c8b0cf0fa286e32a9ceefd82692bd9a778f
Patricia <82049947+Patrusky@users.noreply.github.com>
2022-08-06 12:58:52 +0200

    Comentarios incluidos y offset añadido en la url de comics

--- Proyecto-Marvel/src/app/services/comics.service.ts
+++ Proyecto-Marvel/src/app/services/comics.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-//Importamos el environment , que es el que tiene las partes de la url
 
 @Injectable({
   providedIn: 'root'
@@ -15,11 +14,15 @@ export class ComicsService {
 
    }
    getAll(){
-    return this.http.get(`${environment.url}${this.endpoint}?ts=2&apikey=${environment.PUBLIC_KEY}&hash=${environment.HASH}`)
+    return this.http.get(`${environment.url}${this.endpoint}?ts=2&apikey=${environment.PUBLIC_KEY}&hash=${environment.HASH}&offset=2`)
     .pipe(map((data:any) => {
       return data.data.results;
     }));
    }
 }
-/* Con .pipe estamos haciendo un filtro a los datos para que vengan limpios y con map estamos devolviendo
+/* ********** COMENTARIOS ********** */
+/*
+En esta llamada la url viene importada de environments, allí se han creado las variables de url, key y hash. Endpoint
+ es otra parte de la url, que en este caso contiene comics.
+Con .pipe estamos haciendo un filtro a los datos para que vengan limpios y con map estamos devolviendo
 un nuevo array con esos datos limpios.  */
--- Proyecto-Marvel/src/app/services/personajes.service.ts
+++ Proyecto-Marvel/src/app/services/personajes.service.ts
@@ -23,7 +23,6 @@ export class PersonajesService {
   } */
   getByName(nombre: string): Observable<any> {
     let url: string = "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith="+ nombre +"&ts=2&apikey=597e5f5ab2c799f16c0b4c25bcdc2eac&hash=0933ce7dab464a357a216f91dc030325";
-
     return this.http.get(url);
   }
   getById(id: number): Observable<any> {
@@ -34,3 +33,12 @@ export class PersonajesService {
 
 }
 
+/* ********** COMENTARIOS ********** */
+/*
+getAll -->  Hacemos la llamada para traer todos los datos.
+
+getByName --> Hacemos la llamada pero solo traemos los nombres de los personajes. Esto se consigue porque ponemos la clave valor que nos indica la api. Tras el query (?) de characters va  nameStartsWith(clave) = "+ nombre +"(valor).
+Usamos esta clave porque no es tan específica, con la de name tendríamos que escribir el nombre exacto de la api.
+
+getById --> Hacemos la llamda y traemos los id de los personajes. Aquí no piden que pongamos el valor (" + id + ") tras el path(\) de characters.
+*/
