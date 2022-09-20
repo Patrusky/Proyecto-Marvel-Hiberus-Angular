2df2ed6c7e1da02614a2952ea395eb3480f4ee34
Patricia <82049947+Patrusky@users.noreply.github.com>
2022-09-20 22:41:20 +0200

    Deploy gb_pages

--- Proyecto-Marvel/src/environments/environment.prod.ts
+++ Proyecto-Marvel/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  PUBLIC_KEY :'597e5f5ab2c799f16c0b4c25bcdc2eac',
+  HASH: '0933ce7dab464a357a216f91dc030325',
+  url: 'https://gateway.marvel.com:443/v1/public/',
 };
